1c656dcb7c6e39d9b3cc5d7b43f0cb1f782d17a4
claudim <claudimarco@hotmail.it>
2021-03-25 10:28:22 +0100

    Added some comments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
+//******************************************************************************
+// File Description :
+// Main file for the generation of a hex mesh using Gmsh library.
+//******************************************************************************
+
 #include <iostream>
 #include "Hex_dom_gmsh_mesh.h"
 
 
 /**
  * The command line arguments are handled using main() function arguments where argc refers to the number of arguments passed, and argv[] is a pointer array which points to each argument passed to the program.
- * @return
  */
 int main(int argc, char *argv[] ) {
 
@@ -14,23 +18,8 @@ int main(int argc, char *argv[] ) {
         std::string outputFileName = argv[2];
         double maxMeshElementSize = std::stod(argv[3]);
 
-        std::cout << inputFileName << std::endl;
-        std::cout << outputFileName << std::endl;
         Hex_dom_gmsh_mesh gmsh_mesher;
-
-        // Get starting timepoint
-        auto start_reading = std::chrono::high_resolution_clock::now();
-
         gmsh_mesher.run(inputFileName, outputFileName, maxMeshElementSize);
-
-        // Get ending timepoint
-        auto stop_reading = std::chrono::high_resolution_clock::now();
-
-        // Get duration
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop_reading - start_reading);
-
-        std::cout << "Time taken by read function is : "
-                  << duration.count() << " microseconds" << std::endl;
 
     }
     else if( argc > 4 ) {
